cd2417928e9cec75b7ea35259167cbd9a6418dd1
Rishikasai <48752586+Rishikasai@users.noreply.github.com>
2019-10-09 14:33:22 +0530

    Create main.go

---
+++ Palindrome/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	s "strings"
+)
+
+func main() {
+	a := palindrome("maNdam")
+	if a == "Y" {
+		fmt.Println("is a palindrome")
+	}
+	if a == "N" {
+		fmt.Println("is not a palindrome")
+	}
+}
+func palindrome(a string) string {
+	b := []byte{}
+	for i := len(a) - 1; i >= 0; i-- {
+		b = append(b, a[i])
+	}
+	c := string(b)
+	fmt.Println(c)
+	d := s.ToLower(a)
+	e := s.ToLower(c)
+	if d == e {
+		return "Y"
+	}
+
+	return "N"
+
+}
